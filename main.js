bac5fe510bd13939f6fb93a56973465716953f34
wesbasinger <wbasinger@villagetechschools.org>
2016-12-19 12:06:05 -0600

    It worksgit add -A .

---
+++ main.js
@@ -0,0 +1,35 @@
+var _ = require("lodash");
+var Plotly = require("plotly.js");
+
+var data = require("./data");
+
+var weeks = _.range(2, 37);
+
+var alg2Results = [];
+var engResults = [];
+
+weeks.forEach(function(weekInt) {
+  for(var i=0; i<data.length; i++) {
+    if(data[i].weekNumber == weekInt) {
+      alg2Results.push(data[i].alg2Rate);
+      engResults.push(data[i].engRate);
+      break;
+    }
+  }
+});
+
+var trace1 = {
+  x: weeks.slice(0,alg2Results.length),
+  y: alg2Results,
+  type: 'line'
+};
+
+var trace2 = {
+  x: weeks.slice(0,engResults.length),
+  y: engResults,
+  type: 'line'
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('plot-results', data);
